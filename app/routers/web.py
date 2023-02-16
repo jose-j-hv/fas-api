4b4ff007f7e074d57184d0ca8a46eff3ea0255ce
jose-j-hv <josejuanhv9512@gmil.com>
2023-02-16 02:21:46 -0600

    Generando formulario

---
+++ app/routers/web.py
@@ -0,0 +1,39 @@
+from fastapi import Request, APIRouter
+from starlette.templating import Jinja2Templates
+import aiohttp
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory="app/templates")
+
+url = "http://127.0.0.1:8000"
+
+@router.get("/")
+def home(request: Request):
+    msj = ""
+    return templates.TemplateResponse("home.html",{"request":request, "msj":msj })
+
+@router.post("/registrer")
+async def registration(request: Request):
+    form = await request.form()
+    usuario = {
+        "username":form.get('username'),
+        "password":form.get('password'),
+        "nombre":form.get('nombre'),
+        "apellido":form.get('apellido'),
+        "direccion":form.get('direccion'),
+        "correo":form.get('correo'),
+        "telefono":form.get('telefono')
+    }
+    url_post= f"{url}/user/"
+    async with aiohttp.ClientSession() as session:
+        response = await session.request(method="POST",url=url_post, json=usuario)
+        response_json = await response.json()
+        print("final-->",response_json)
+        if "respuesta:" in response_json:
+            msj = "Usuario creado correctamente."
+            type_alert ="primary"
+        else: 
+            msj = "Usuario no creado."
+            type_alert ="danger"
+        return templates.TemplateResponse("create_user.html",{"request":request, "msj":msj, "type_alert":type_alert })
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI 
 import uvicorn
 from app.db.database import Base,engine 
-from app.routers import user, auth
+from app.routers import user, auth, web
 
 #def create_tables():
 #    Base.metadata.create_all(bind=engine)
@@ -9,8 +9,9 @@ from app.routers import user, auth
 
 app = FastAPI()
 app.include_router(user.router)
-
 app.include_router(auth.router)
+app.include_router(web.router)
 
 if __name__=="__main__":
     uvicorn.run("main:app",port=8000,reload=True)
+    
